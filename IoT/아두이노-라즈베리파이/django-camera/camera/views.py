675e1980b8db14ff5f9f3249ce6cc58d32e3f434
박인남 <pin954562@gmail.com>
2020-08-03 11:46:02 +0900

    [Chore] change static derectory,S03P13B109-12

--- IoT/아두이노-라즈베리파이/django-camera/camera/views.py
+++ IoT/아두이노-라즈베리파이/django-camera/camera/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from picamera import PiCamera
 
 # Create your views here.
 def index(request):
+    camera = PiCamera()
+    camera.capture('static/image/image.jpg')
     return render(request,'index.html')
