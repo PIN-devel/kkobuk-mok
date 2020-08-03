dd00a8da1458bf420f7109eecbab0e73f8edc908
조수미 <tnal1632@naver.com>
2020-08-03 16:49:16 +0900

    [Feat] exception message to mattermost, S03P13B109-9

---
+++ backend/accounts/utils.py
@@ -0,0 +1,18 @@
+from rest_framework.views import exception_handler
+import requests, json
+from matterhook import Webhook
+
+def custom_exception_handler(exc, context):
+    # Call REST framework's default exception handler first,
+    # to get the standard error response.
+    response = exception_handler(exc, context)
+    # Now add the HTTP status code to the response.
+    if response is not None:
+        response.data['status_code'] = response.status_code
+
+    if response.data['status_code']//100 == 5:
+        mwh = Webhook('https://meeting.ssafy.com', 'i8gbyaejzfrn98oypq5xsmao6w')
+        json_data = json.dumps(response.data, ensure_ascii=False)
+        mwh.send(json_data)
+
+    return response
