852f899676d82bb68d69b989ea89fa6c90ebb5ed
bright327 <bright327@naver.com>
2020-08-05 17:26:59 +0900

    [Feat] intergrate rasp & ardu,S03P13B109-38

--- IoT/아두이노-라즈베리파이/django-camera/camera/views.py
+++ IoT/아두이노-라즈베리파이/django-camera/camera/views.py
@@ -1,21 +1,67 @@
-from django.shortcuts import render
-from picamera import PiCamera
-from .tensorflow.tensorflow import outPrint
-################
-import tensorflow.keras
-from PIL import Image, ImageOps
-import numpy as np
-import os
+import serial,time
 
+from django.shortcuts import render
+
+from picamera import PiCamera
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from .TeachableMachine.tensorflow import outPrint
+
+
+arduino = serial.Serial("/dev/ttyACM0",9600)
 
 def index(request):
+    return render(request,'index.html')
+
+@api_view(['GET'])
+def take_pic(request):
     img_src= 'static/image/image.jpg'
     camera = PiCamera()
     camera.resolution = (224, 224)
     camera.capture(img_src)
     camera.close()
-    context={
-        "result" : outPrint(img_src)     
+    tmpList = outPrint(img_src)[0]
+    max_val = -1
+    for i in range(3):
+        if max_val<tmpList[i]:
+            max_val=tmpList[i]
+            idx=i+1
+    cmd = "MC"+str(idx)
+    arduino.write(cmd.encode())
+    if arduino.readable():
+        tmp = arduino.readline()
+        motor = tmp.decode('utf-8')[:len(tmp)-3]
+    arduino.write('SR'.encode())
+    if arduino.readable():
+        tmp = arduino.readline()
+        sensor = tmp.decode('utf-8')[2:len(tmp)-3]
+    context = {
+        "motor":motor,
+        "sensor":sensor
     }
-    
-    return render(request,'index.html',context)
+    return Response(context)
+
+@api_view(['GET'])
+def sensor_read(request):
+    arduino.write('SR'.encode())
+    if arduino.readable():
+        tmp = arduino.readline()
+        res = tmp.decode('utf-8')[2:len(tmp)-1]
+    context={
+        'res':res
+    }
+    return Response(context)
+
+@api_view(['GET'])
+def motor_control(request,level):
+    cmd = "MC"+str(level)
+    arduino.write(cmd.encode())
+    if arduino.readable():
+        tmp = arduino.readline()
+        res = tmp.decode('utf-8')[:len(tmp)-1]
+    context={
+        'res':res
+    }
+    return Response(context)
