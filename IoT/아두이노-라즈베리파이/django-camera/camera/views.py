b923e5dd97cc30f1a7b7c33ca0ee0383ab608fa3
bright327 <bright327@naver.com>
2020-08-05 14:12:33 +0900

    temp

--- IoT/아두이노-라즈베리파이/django-camera/camera/views.py
+++ IoT/아두이노-라즈베리파이/django-camera/camera/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from picamera import PiCamera
+from .tensorflow.tensorflow import outPrint
+################
+import tensorflow.keras
+from PIL import Image, ImageOps
+import numpy as np
+import os
 
-# Create your views here.
+
 def index(request):
+    img_src= 'static/image/image.jpg'
     camera = PiCamera()
-    camera.capture('static/image/image.jpg')
-    return render(request,'index.html')
+    camera.resolution = (224, 224)
+    camera.capture(img_src)
+    camera.close()
+    context={
+        "result" : outPrint(img_src)     
+    }
+    
+    return render(request,'index.html',context)
