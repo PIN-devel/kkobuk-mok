d335449859fa1ef5ebc144ff03a9d1606530444a
bright327 <pin954562@gmail.com>
2020-08-10 03:07:57 +0100

    [Optimize] ai prediction time optimization,S03P13B109-60

--- IoT/아두이노-라즈베리파이/django-camera/camera/views.py
+++ IoT/아두이노-라즈베리파이/django-camera/camera/views.py
@@ -8,23 +8,44 @@ from picamera import PiCamera
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-from .TeachableMachine.tensorflow import outPrint
-
+# from .TeachableMachine.tensorflow import outPrint
+import tensorflow.keras
+from PIL import Image, ImageOps
+import numpy as np
 
 arduino = serial.Serial("/dev/ttyACM0",9600)
+np.set_printoptions(suppress=True)
+model = tensorflow.keras.models.load_model('keras_model.h5')
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+size = (224, 224)
+
+
+
 
 def index(request):
     return render(request,'index.html')
 
 @api_view(['GET'])
 def take_pic(request):
+
+    start = time.time()
+
     img_src= 'static/image/image.jpg'
     camera = PiCamera()
     camera.resolution = (224, 224)
     time.sleep(5)
     camera.capture(img_src)
     camera.close()
-    tmpList = outPrint(img_src)[0]
+    print("카메라 촬영 종료 :", time.time() - start)
+    image = Image.open(img_src)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+    image_array = np.asarray(image)
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    data[0] = normalized_image_array
+    print("이미지 프로세싱 종료 :", time.time() - start)
+    prediction = model.predict(data)
+    tmpList = prediction[0]
+    print("ai예측 종료 :", time.time() - start)
     max_val = -1
     for i in range(3):
         if max_val<tmpList[i]:
