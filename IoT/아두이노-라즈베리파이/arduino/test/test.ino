c683cef050ed9232ef6349bda9b16e59a538cd1f
bright327 <pin954562@gmail.com>
2020-08-17 12:45:54 +0100

    [Feat] add alarm theme,S03P13B109-103

--- IoT/아두이노-라즈베리파이/arduino/test/test.ino
+++ IoT/아두이노-라즈베리파이/arduino/test/test.ino
@@ -10,142 +10,253 @@
 #define DHTTYPE DHT11
 DHT_Unified dht(DHT_PIN, DHTTYPE);
 
-int Tones[7] = {261, 294, 330, 349, 392, 440, 494};
+int Oct4[12] = {262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494};
+int Oct5[12] = {523, 554, 587, 622, 659, 698, 740, 784, 831, 880, 932, 988};
+int Oct6[12] = {1047, 1109, 1175, 1245, 1319, 1397, 1480, 1568, 1661, 1760, 1865, 1976};
 
 Servo servo;
-
 
 boolean b_new_data = false;
 String s_data;
 boolean humidifier_state = false;
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
   Serial.begin(9600);
+  pinMode(RELAY_PIN, OUTPUT);
   servo.attach(SERVO_PIN);
   dht.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
   recData();
-  function ();
+  function();
 }
-void recData(){
-  if(Serial.available() > 0) { 
+void recData()
+{
+  if (Serial.available() > 0)
+  {
     s_data = Serial.readString();
     Serial.print(s_data);
     b_new_data = true;
   }
 }
-void function(){
-  if (b_new_data == true){
+void note(int pin, int hz, int t)
+{
+  tone(pin, hz);
+  delay(t);
+}
+void staNote(int pin, int hz, int t)
+{
+  tone(pin, hz);
+  delay(t - 20);
+  noTone(pin);
+  delay(20);
+}
+void rest(int pin, int t)
+{
+  noTone(pin);
+  delay(t);
+}
+void function()
+{
+  if (b_new_data == true)
+  {
 
-    if (s_data == "SR"){
-        sensors_event_t event;
-        dht.temperature().getEvent(&event);
-        
-        if (isnan(event.temperature)) {
-          Serial.println(F("Error reading temperature!"));
-        }
-        else {
-          
-          Serial.print(event.temperature);
-          Serial.print(",");
-        }
-        // Get humidity event and print its value.
-        dht.humidity().getEvent(&event);
-        if (isnan(event.relative_humidity)) {
-          Serial.println(F("Error reading humidity!"));
-        }
-        else {
-          Serial.println(event.relative_humidity);
-        }
+    if (s_data == "SR")
+    {
+      sensors_event_t event;
+      dht.temperature().getEvent(&event);
+
+      if (isnan(event.temperature))
+      {
+        Serial.println(F("Error reading temperature!"));
+      }
+      else
+      {
+
+        Serial.print(event.temperature);
+        Serial.print(",");
+      }
+      // Get humidity event and print its value.
+      dht.humidity().getEvent(&event);
+      if (isnan(event.relative_humidity))
+      {
+        Serial.println(F("Error reading humidity!"));
+      }
+      else
+      {
+        Serial.println(event.relative_humidity);
+      }
     }
-    
-    else if(s_data == "MC1"){
-      servo.write(0);
+
+    else if (s_data == "MC1")
+    {
+      servo.write(90);
       Serial.println("1");
     }
-    
-    else if(s_data == "MC2"){
-      servo.write(90);
+
+    else if (s_data == "MC2")
+    {
+      servo.write(45);
       Serial.println("2");
     }
-    
-    else if(s_data == "MC3"){
-      servo.write(180);
-      Serial.println("3");      
+
+    else if (s_data == "MC3")
+    {
+      servo.write(0);
+      Serial.println("3");
     }
-    
-    else if(s_data == "SP"){
-      tone(SPEAKER_PIN,1865);
+    else if (s_data == "MS1")
+    {
+    }
+    else if (s_data == "ME1")
+    {
+    }
+    else if (s_data == "MW1")
+    {
+      staNote(SPEAKER_PIN, Oct5[6], 180);
+      staNote(SPEAKER_PIN, Oct5[9], 180);
+      staNote(SPEAKER_PIN, Oct5[9], 180);
+      staNote(SPEAKER_PIN, Oct5[9], 180);
+      rest(SPEAKER_PIN, 180);
+      staNote(SPEAKER_PIN, Oct5[6], 180);
+      staNote(SPEAKER_PIN, Oct5[9], 360);
+
+      staNote(SPEAKER_PIN, Oct5[6], 180);
+      staNote(SPEAKER_PIN, Oct5[9], 180);
+      staNote(SPEAKER_PIN, Oct5[9], 180);
+      staNote(SPEAKER_PIN, Oct5[9], 360);
+      staNote(SPEAKER_PIN, Oct5[11], 180);
+      staNote(SPEAKER_PIN, Oct5[9], 360);
+
+      staNote(SPEAKER_PIN, Oct6[2], 180);
+      staNote(SPEAKER_PIN, Oct6[2], 180);
+      staNote(SPEAKER_PIN, Oct6[2], 180);
+      rest(SPEAKER_PIN, 180);
+      staNote(SPEAKER_PIN, Oct6[2], 180);
+      staNote(SPEAKER_PIN, Oct6[2], 180);
+      staNote(SPEAKER_PIN, Oct6[2], 180);
+      rest(SPEAKER_PIN, 180);
+
+      staNote(SPEAKER_PIN, Oct6[2], 180);
+      staNote(SPEAKER_PIN, Oct5[11], 180);
+      staNote(SPEAKER_PIN, Oct5[9], 180);
+      staNote(SPEAKER_PIN, Oct5[11], 360);
+      staNote(SPEAKER_PIN, Oct5[9], 180);
+      staNote(SPEAKER_PIN, Oct5[11], 180);
+
+      staNote(SPEAKER_PIN, Oct5[7], 180);
+      staNote(SPEAKER_PIN, Oct5[7], 180);
+      staNote(SPEAKER_PIN, Oct5[9], 180);
+      staNote(SPEAKER_PIN, Oct5[9], 180);
+      staNote(SPEAKER_PIN, Oct5[11], 180);
+      staNote(SPEAKER_PIN, Oct5[11], 180);
+      staNote(SPEAKER_PIN, Oct6[1], 180);
+      staNote(SPEAKER_PIN, Oct6[2], 360);
+
+      staNote(SPEAKER_PIN, Oct5[10], 360);
+      staNote(SPEAKER_PIN, Oct5[10], 540);
+      rest(SPEAKER_PIN, 360);
+
+      staNote(SPEAKER_PIN, Oct6[6], 180);
+      staNote(SPEAKER_PIN, Oct6[6], 180);
+      staNote(SPEAKER_PIN, Oct6[6], 180);
+      staNote(SPEAKER_PIN, Oct6[6], 360);
+      staNote(SPEAKER_PIN, Oct6[4], 180);
+      staNote(SPEAKER_PIN, Oct6[6], 360);
+      staNote(SPEAKER_PIN, Oct6[4], 360);
+    }
+
+    else if (s_data == "MS2")
+    {
+    }
+    else if (s_data == "ME2")
+    {
+    }
+    else if (s_data == "MW2")
+    {
+    }
+
+    else if (s_data == "MS3")
+    {
+    }
+    else if (s_data == "ME3")
+    {
+    }
+    else if (s_data == "MW3")
+    {
+      tone(SPEAKER_PIN, 1865);
       delay(2000);
-      
-      tone(SPEAKER_PIN,988);
+
+      tone(SPEAKER_PIN, 988);
       delay(500);
-      tone(SPEAKER_PIN,1175);
+      tone(SPEAKER_PIN, 1175);
       delay(500);
-      tone(SPEAKER_PIN,1480);
+      tone(SPEAKER_PIN, 1480);
       delay(500);
-      tone(SPEAKER_PIN,1175);
+      tone(SPEAKER_PIN, 1175);
       delay(500);
 
-      tone(SPEAKER_PIN,932);
+      tone(SPEAKER_PIN, 932);
       delay(500);
-      tone(SPEAKER_PIN,1109);
+      tone(SPEAKER_PIN, 1109);
       delay(500);
-      tone(SPEAKER_PIN,1319);
+      tone(SPEAKER_PIN, 1319);
       delay(500);
-      tone(SPEAKER_PIN,932);
-      delay(500);
-
-            tone(SPEAKER_PIN,988);
-      delay(500);
-      tone(SPEAKER_PIN,1175);
-      delay(500);
-      tone(SPEAKER_PIN,1480);
-      delay(500);
-      tone(SPEAKER_PIN,1175);
+      tone(SPEAKER_PIN, 932);
       delay(500);
 
-      tone(SPEAKER_PIN,932);
+      tone(SPEAKER_PIN, 988);
       delay(500);
-      tone(SPEAKER_PIN,1109);
+      tone(SPEAKER_PIN, 1175);
       delay(500);
-      tone(SPEAKER_PIN,1319);
+      tone(SPEAKER_PIN, 1480);
       delay(500);
-      tone(SPEAKER_PIN,932);
+      tone(SPEAKER_PIN, 1175);
       delay(500);
-      
-   
+
+      tone(SPEAKER_PIN, 932);
+      delay(500);
+      tone(SPEAKER_PIN, 1109);
+      delay(500);
+      tone(SPEAKER_PIN, 1319);
+      delay(500);
+      tone(SPEAKER_PIN, 932);
+      delay(500);
+
       noTone(SPEAKER_PIN);
-      Serial.println("알람");      
+      Serial.println("알람");
     }
-    else if(s_data == "RON"){
-      if (!humidifier_state){
-        digitalWrite(RELAY_PIN,1);
+    else if (s_data == "RON")
+    {
+      if (!humidifier_state)
+      {
+        digitalWrite(RELAY_PIN, 1);
         delay(200);
-        digitalWrite(RELAY_PIN,0);
-         
+        digitalWrite(RELAY_PIN, 0);
+
         humidifier_state = true;
       }
-      Serial.println("가습기 : on");     
+      Serial.println("가습기 : on");
     }
-    else if(s_data == "ROF"){
-      if (humidifier_state){
-        digitalWrite(RELAY_PIN,1);
+    else if (s_data == "ROF")
+    {
+      if (humidifier_state)
+      {
+        digitalWrite(RELAY_PIN, 1);
         delay(200);
-        digitalWrite(RELAY_PIN,0);
+        digitalWrite(RELAY_PIN, 0);
         delay(200);
-        digitalWrite(RELAY_PIN,1);
+        digitalWrite(RELAY_PIN, 1);
         delay(200);
-        digitalWrite(RELAY_PIN,0);
-        
+        digitalWrite(RELAY_PIN, 0);
+
         humidifier_state = false;
       }
-      digitalWrite(RELAY_PIN,0);
-      Serial.println("가습기 : off");      
+      digitalWrite(RELAY_PIN, 0);
+      Serial.println("가습기 : off");
     }
 
     b_new_data = false;
